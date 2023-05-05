7c9a3ab164141692b865e06268b898ffd3eda5fa
nuleongee <nuleongee@gmail.com>
2023-05-05 23:58:49 +0900

    feat: Profile 테스트 추가

--- src/components/Profile.test.js
+++ src/components/Profile.test.js
@@ -6,4 +6,10 @@ describe('<Profile />', () => {
     const utils = render(<Profile username="nuleongee" name="누렁이" />);
     expect(utils.container).toMatchSnapshot();
   });
+  it('shows the props correctly', () => {
+    const utils = render(<Profile username="nuleongee" name="누렁이" />);
+    utils.getByText('nuleongee'); // velopert 라는 텍스트를 가진 엘리먼트가 있는지 확인
+    utils.getByText('(누렁이)'); // (김민준) 이라는 텍스트를 가진 엘리먼트가 있는지 확인
+    utils.getByText(/누/); // 정규식 /김/ 을 통과하는 엘리먼트가 있는지 확인
+  });
 });
