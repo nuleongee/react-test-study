a159a395d855402d85d9ac2b1d3bbd1136cdb2c4
Subin Lim <sblim@i-screamedu.co.kr>
2023-05-15 00:29:17 +0900

    test: TodoForm 완료

--- src/components/TodoForm/TodoForm.test.js
+++ src/components/TodoForm/TodoForm.test.js
@@ -3,21 +3,46 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import TodoForm from './index.jsx';
 
 describe('<TodoForm/>', () => {
-  it('has input and a button', () => {
-    render(<TodoForm />);
+  const setup = props => {
+    const utils = render(<TodoForm {...props} />);
+    const input = screen.getByPlaceholderText('할 일을 입력하세요');
+    const button = screen.getByText('등록');
+    return {
+      ...utils,
+      input,
+      button,
+    };
+  };
 
-    screen.getByPlaceholderText('할 일을 입력하세요');
-    screen.getByText('등록');
+  it('has input and a button', () => {
+    const { input, button } = setup();
+    expect(input).toBeTruthy();
+    expect(button).toBeTruthy();
   });
 
   it('changes input', () => {
-    render(<TodoForm />);
-    const input = screen.getByPlaceholderText('할 일을 입력하세요');
+    const { input } = setup();
     fireEvent.change(input, {
       target: {
         value: 'TDD 배우기',
       },
     });
     expect(input.value).toBe('TDD 배우기');
+    expect(input).toHaveAttribute('value', 'TDD 배우기'); // 속성값 존재 여부 확인
+  });
+
+  it('calls inInsert and clears input', () => {
+    const onInsert = jest.fn();
+    const { input, button } = setup({ onInsert }); // props 가 필요 할땐 직접 파라미터로 전달
+    // 수정
+    fireEvent.change(input, {
+      target: {
+        value: 'TDD 배우기',
+      },
+    });
+    // 버튼 클릭
+    fireEvent.click(button);
+    expect(onInsert).toBeCalledWith('TDD 배우기'); // onInsert가 'TDD 배우기' 파라미터가 호출됐어야함
+    expect(input).toHaveAttribute('value', ''); // input이 비워져야함
   });
 });
--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
-const TodoForm = () => {
-  return <form>
-    <input placeholder="할 일을 입력하세요"/>
-    <button type="submit">등록</button>
-  </form>;
+const TodoForm = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+  const onChange = useCallback(e => {
+    setValue(e.target.value);
+  }, []);
+  const onSubmit = useCallback(
+    e => {
+      onInsert(value);
+      setValue('');
+      e.preventDefault(); // 새로고침 방지
+    },
+    [onInsert, value],
+  );
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input placeholder="할 일을 입력하세요" value={value} onChange={onChange} />
+      <button type="submit">등록</button>
+    </form>
+  );
 };
 
 export default TodoForm;
