35b8ccd67789b5433effe51a5f3ccadfaaac3fa2
Subin Lim <sblim@i-screamedu.co.kr>
2023-05-17 23:51:28 +0900

    feat: TodoList 추가

---
+++ src/components/TodoList/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import TodoItem from '../TodoItem/index.jsx';
+
+const TodoList = ({ todos, onToggle, onRemove }) => {
+  return (
+    <ul>
+      {todos.map(todo => (
+        <TodoItem todo={todo} key={todo.id} onToggle={onToggle} onRemove={onRemove} />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
